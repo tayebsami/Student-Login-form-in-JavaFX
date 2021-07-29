b73b52a2f82e9528db76504d80a960d8a05b1799
tayebsami <tayebsami@gmail.com>
2021-07-29 21:36:46 +0100

    Added student choice interface after login

---
+++ src/Controllers/StudentOptionController.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StudentOptionController {
+
+
+}
